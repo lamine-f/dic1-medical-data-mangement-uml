a9c023e0409edd225de911769b06160c8bc3b574
lamine-f <mouhamedlaminefaye77@gmail.com>
2024-03-21 10:39:38 +0000

    adding spring persistence module

---
+++ persistence/src/test/java/esp/lord/dic1/uml/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package esp.lord.dic1.uml.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
