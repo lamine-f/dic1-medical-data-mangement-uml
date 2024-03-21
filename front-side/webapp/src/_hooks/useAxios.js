9df5079a6b44498937bde3c2c479c2bb18b40a54
lamine-f <mouhamedlaminefaye77@gmail.com>
2024-03-21 16:32:21 +0000

    init webapp && refactoring

---
+++ front-side/webapp/src/_hooks/useAxios.js
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+
+const useAxiosFunction = () => {
+
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [controller, setController] = useState();
+    const axiosFetch = async (configObj)  => {
+        setLoading(true);
+        const ctrl = new AbortController();
+        setController(ctrl);
+
+        const {
+            axiosInstance,
+            method,
+            url,
+            requestConfig = []
+        } = configObj;
+
+        await  axiosInstance[method.toLowerCase()](url,
+            ...requestConfig
+        ).then((res) => {
+            setResponse(res.data);
+        }).catch((err) => {
+            setError(err?.response?.data || "Error Server");
+        }).finally (() => {
+            setLoading(false);
+        })
+    }
+    useEffect(() => {
+        if (controller)
+            return () => controller.abort()
+    }, [controller])
+    return [response, error, loading, axiosFetch];
+}
+export default useAxiosFunction;
